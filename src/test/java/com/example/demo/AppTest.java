60e7597494ab3882a01df0827e8f0bcdd33ddac1
Vasi1i <7pwrrr@gmail.com>
2023-12-29 14:25:45 +0300

    c2

---
+++ src/test/java/com/example/demo/AppTest.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTest {
+
+    @Test
+    void t1() {
+        System.out.println("t1");
+    }
+
+}
