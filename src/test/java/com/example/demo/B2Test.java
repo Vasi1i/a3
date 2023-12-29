356d1a97a37830a0da934d6963f3a0ac34f60dc5
Vasi1i <7pwrrr@gmail.com>
2023-12-29 14:50:24 +0300

    bc-c3

--- src/test/java/com/example/demo/B2Test.java
+++ src/test/java/com/example/demo/B2Test.java
@@ -1,10 +1,16 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class B2Test {
 
     String S = "s";
+
+    @Test
+    void t1() {
+        System.out.println("t1");
+    }
 
 }
